3d74c96d0b10174d11db550dbb72139dd7ed7738
Allahuddin khedmat <160042193+alawoddin@users.noreply.github.com>
2026-07-27 22:10:04 +0430

    the three js is add 2 two way

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// import * as THREE from "./three.module.min"
+// import * as THREE from "./three.module.min.js";
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
+
+
+console.log(THREE);
